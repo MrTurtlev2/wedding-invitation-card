227b1826d5ebd17dd1a1bd851c75a1ac1f22519a
Marek <wojnarmarek0@gmail.com>
2026-08-15 02:19:23 +0200

    info tiles svg

--- src/components/InfoTile/InfoTile.tsx
+++ src/components/InfoTile/InfoTile.tsx
@@ -5,12 +5,10 @@ interface InfoTileProps {
 }
 
 const InfoTile = ({type}: InfoTileProps) => {
-
-    
     return (
         <div className='info-main-wrapper'>
-            <img src={`/assets/info/${type}.png`} alt={''}/>
-
+            <img src={`/assets/info-tiles/${type}.png`} alt={''}/>
+            <img src={`/assets/info-tiles-icons/${type}-icon.svg`} alt={''}/>
         </div>
     )
 }
