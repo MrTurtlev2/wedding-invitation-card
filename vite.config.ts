b2209685e16e84eed6c65cbea91f72eea93c91e3
Marek <wojnarmarek0@gmail.com>
2026-08-17 21:43:06 +0200

    pwa & local fonts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,34 @@
 import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from "@tailwindcss/vite";
+import {VitePWA} from "vite-plugin-pwa";
 
-// https://vite.dev/config/
 export default defineConfig({
-    plugins: [react(), tailwindcss()],
+    plugins: [react(), tailwindcss(),
+        VitePWA({
+            registerType: 'autoUpdate',
+
+            manifest: {
+                name: 'Weronika & Marek',
+                short_name: 'W&M',
+                description: 'Wedding Weekend nad jeziorem Como',
+                theme_color: '#FFFFFF',
+                background_color: '#FFFFFF',
+                display: 'standalone',
+
+                icons: [
+                    {
+                        src: '/icons/icon-192.png',
+                        sizes: '192x192',
+                        type: 'image/png',
+                    },
+                    {
+                        src: '/icons/icon-512.png',
+                        sizes: '512x512',
+                        type: 'image/png',
+                    },
+                ],
+            },
+        }),
+    ],
 })
