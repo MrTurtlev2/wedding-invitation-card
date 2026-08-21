6be8495a010952becdeff260b25203fbfd958652
Marek <wojnarmarek0@gmail.com>
2026-08-21 20:39:10 +0200

    vite config

--- vite.config.ts
+++ vite.config.ts
@@ -15,20 +15,8 @@ export default defineConfig({
                 theme_color: '#FFFFFF',
                 background_color: '#FFFFFF',
                 display: 'standalone',
-
-                icons: [
-                    {
-                        src: '/icons/icon-192.png',
-                        sizes: '192x192',
-                        type: 'image/png',
-                    },
-                    {
-                        src: '/icons/icon-512.png',
-                        sizes: '512x512',
-                        type: 'image/png',
-                    },
-                ],
             },
         }),
     ],
+    base: '/wedding-invitation-card/'
 })
