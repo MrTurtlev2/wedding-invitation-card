45d4d00ef04e85e59a1c39415319e9df7b006387
Marek <wojnarmarek0@gmail.com>
2026-08-21 20:34:04 +0200

    Updates

---
+++ registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/wedding-invitation-card/sw.js', { scope: '/wedding-invitation-card/' })})}
